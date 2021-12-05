fa7cee91dcbd2af676267d7492a2797c3d385f93
Emre Bayrak <eemmrree0@outlook.com>
2021-12-05 17:34:47 +0300

    development branch add

--- NorthwindAngular/src/app/app.component.ts
+++ NorthwindAngular/src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NorthwindAngular';
+  user:string = 'Emre Bayrak';
+  product1 = { productId: 1, productName: 'Elbise', categoryId: 1, unitsInPrice: 2 }
+  product2 = { productId: 2, productName: 'Bilgisayar', categoryId: 1, unitsInPrice: 20000 }
+  product3 = { productId: 3, productName: 'Elma', categoryId: 1, unitsInPrice: 20 }
+  product4 = { productId: 4, productName: 'Kulaklık', categoryId: 1, unitsInPrice: 2000 }
+  product5 = { productId: 5, productName: 'Ayakkabı', categoryId: 1, unitsInPrice: 200 }
+
+  products = [this.product1, this.product2, this.product3, this.product4, this.product5];
 }
